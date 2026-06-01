06d27f049fe7090d2cf44ec9fca47b01a8a0cf2e
HaideS14 <haidesanchezgut@gmail.com>
2026-05-31 21:34:36 -0600

    Frontend listo para despliegue

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
 
     connectionString:
-    'postgresql://neondb_owner:npg_wur45tlYBadA@ep-flat-silence-aqgzasey-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+    process.env.DATABASE_URL,
 
     ssl: {
         rejectUnauthorized: false
